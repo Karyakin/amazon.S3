96e074b2e55db5603c23cad8bf94922f94996a98
dmitrijkarakin <d.karyakin@richbrains.net@gmail.com>
2023-10-11 21:54:25 +0300

    tmp

--- s3-dotnet-7-webapi/S3.Demo.API/Controllers/BucketsController.cs
+++ s3-dotnet-7-webapi/S3.Demo.API/Controllers/BucketsController.cs
@@ -3,6 +3,7 @@ using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace S3.Demo.API.Controllers
@@ -11,12 +12,12 @@ namespace S3.Demo.API.Controllers
     [ApiController]
     public class BucketsController : ControllerBase
     {
-        private  IAmazonS3 _s3Client;
+        //private  IAmazonS3 _s3Client;
         private readonly IConfiguration _configuration;
 
-        public BucketsController(IAmazonS3 s3Client, IConfiguration configuration)
+        public BucketsController(/*IAmazonS3 s3Client,*/ IConfiguration configuration)
         {
-            _s3Client = s3Client;
+            //_s3Client = s3Client;
             _configuration = configuration;
         }
 
@@ -24,24 +25,54 @@ namespace S3.Demo.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateBucketAsync(string bucketName)
         {
-            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
-            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
-            await _s3Client.PutBucketAsync(bucketName);
+            
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            /*var data = await client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => b.BucketName).ToList();
+
+            if (!buckets.Any())
+            {
+                bucketName = "aaa";
+            }*/
+            
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(client, bucketName);
+            
+            if (bucketExists) 
+                return BadRequest($"Bucket {bucketName} already exists.");
+            
+            await client.PutBucketAsync(bucketName);
             return Created("buckets", $"Bucket {bucketName} created.");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllBucketAsync()
         {
-            var data = await _s3Client.ListBucketsAsync();
-            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            var data = await client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => b.BucketName);
             return Ok(buckets);
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteBucketAsync(string bucketName)
         {
-            await _s3Client.DeleteBucketAsync(bucketName);
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            await client.DeleteBucketAsync(bucketName);
             return NoContent();
         }
         
@@ -74,6 +105,7 @@ namespace S3.Demo.API.Controllers
             {
                 ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
 
+                
                 //RegionEndpoint = RegionEndpoint.EUWest1
 
             });
@@ -85,6 +117,11 @@ namespace S3.Demo.API.Controllers
                 {
                     Console.WriteLine(bucket.BucketName);
                 }
+
+                return Ok(new
+                {
+                    response.Buckets
+                });
             }
             catch (AmazonS3Exception e)
             {
@@ -96,6 +133,36 @@ namespace S3.Demo.API.Controllers
             }
         
             return Ok();
+        }
+
+        [HttpPost("createFolderInBucket")]
+        public async Task<IActionResult> CreateFolderInBucket(string bucketName, string folderName)
+        {
+            try
+            {
+                var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+                var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+                using (var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+                       {
+                           ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+                       }))
+                {
+                    var putRequest = new PutObjectRequest
+                    {
+                        BucketName = bucketName,
+                        Key = folderName,
+                        ContentBody = "йцу"
+                    };
+                    await client.PutObjectAsync(putRequest);
+                }
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
--- s3-dotnet-7-webapi/S3.Demo.API/Controllers/FilesController.cs
+++ s3-dotnet-7-webapi/S3.Demo.API/Controllers/FilesController.cs
@@ -1,5 +1,7 @@
-﻿using Amazon.S3;
+﻿using Amazon.Internal;
+using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using S3.Demo.API.Models;
 
@@ -9,19 +11,30 @@ namespace S3.Demo.API.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
-        private readonly IAmazonS3 _s3Client;
+        //private readonly IAmazonS3 _s3Client;
+        private readonly IConfiguration _configuration;
 
-        public FilesController(IAmazonS3 s3Client)
+        public FilesController(/*IAmazonS3 s3Client,*/ IConfiguration configuration)
         {
-            _s3Client = s3Client;
+            //_s3Client = s3Client;
+            _configuration = configuration;
         }
 
 
         [HttpPost]
         public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
         {
-            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(client, bucketName);
+            if (!bucketExists) 
+                return NotFound($"Bucket {bucketName} does not exist.");
+            
             var request = new PutObjectRequest()
             {
                 BucketName = bucketName,
@@ -29,21 +42,31 @@ namespace S3.Demo.API.Controllers
                 InputStream = file.OpenReadStream()
             };
             request.Metadata.Add("Content-Type", file.ContentType);
-            await _s3Client.PutObjectAsync(request);
+            await client.PutObjectAsync(request);
             return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
         {
-            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            
+            
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(client, bucketName);
+            if (!bucketExists) 
+                return NotFound($"Bucket {bucketName} does not exist.");
+            
             var request = new ListObjectsV2Request()
             {
                 BucketName = bucketName,
                 Prefix = prefix
             };
-            var result = await _s3Client.ListObjectsV2Async(request);
+            var result = await client.ListObjectsV2Async(request);
             var s3Objects = result.S3Objects.Select(s =>
             {
                 var urlRequest = new GetPreSignedUrlRequest()
@@ -55,7 +78,7 @@ namespace S3.Demo.API.Controllers
                 return new S3ObjectDto()
                 {
                     Name = s.Key.ToString(),
-                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
+                    PresignedUrl = client.GetPreSignedURL(urlRequest),
                 };
             });
             return Ok(s3Objects);
@@ -64,19 +87,73 @@ namespace S3.Demo.API.Controllers
         [HttpGet("preview")]
         public async Task<IActionResult> GetFileByKeyAsync(string bucketName, string key)
         {
-            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
-            var s3Object = await _s3Client.GetObjectAsync(bucketName, key);
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(client, bucketName);
+            if (!bucketExists) 
+                return NotFound($"Bucket {bucketName} does not exist.");
+            var s3Object = await client.GetObjectAsync(bucketName, key);
             return File(s3Object.ResponseStream, s3Object.Headers.ContentType);
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteFileAsync(string bucketName, string key)
         {
-            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist");
-            await _s3Client.DeleteObjectAsync(bucketName, key);
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+            
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(client, bucketName);
+            
+            if (!bucketExists) 
+                return NotFound($"Bucket {bucketName} does not exist");
+            
+            await client.DeleteObjectAsync(bucketName, key);
             return NoContent();
+        }
+
+        [HttpGet("getFoldersInBucket")]
+        public async Task<IActionResult> GetFoldersInBucket(string bucketName)
+        {
+            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
+            var client = new AmazonS3Client(accessKey, secretKey, new AmazonS3Config
+            {
+                ServiceURL = _configuration.GetSection("AWS:ServiceURL").Value,
+            });
+
+           
+            ListObjectsV2Request request = new ListObjectsV2Request
+            {
+                BucketName = bucketName,
+                Delimiter = "/"
+            };
+
+            ListObjectsV2Response response;
+            do
+            {
+                //var bucketExists = await client.ListObjectsV2Async(request);
+
+                response = await client.ListObjectsV2Async(request);
+
+               var aaa = response.CommonPrefixes;
+                foreach (var commonPrefix in response.CommonPrefixes)
+                {
+                    Console.WriteLine(commonPrefix);
+                }
+
+                request.ContinuationToken = response.NextContinuationToken;
+            } while (response.IsTruncated);
+
+            return Ok(response);
         }
     }
 }
